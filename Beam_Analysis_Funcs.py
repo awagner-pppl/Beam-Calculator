2e7869357bd8c1e1b2c27ab92af5fa233b1ab4f5
awagner-pppl <47219716+awagner-pppl@users.noreply.github.com>
2019-02-12 17:30:02 -0500

    Got unit conversion working

--- Beam_Analysis_Funcs.py
+++ Beam_Analysis_Funcs.py
@@ -2,9 +2,9 @@ import math
 
 def callCalcSection(self):
 
-    print('Step 3')
+    # Determines what calculation to perform based on radio button
 
-    #Determines what calculation to perform based on radio button
+    conFact = unitConv(self)
 
     if self.rectangleButton.isChecked():
 
@@ -18,16 +18,17 @@ def callCalcSection(self):
         else:
 
             self.sectionArea.setValue(
-                self.rectangleOutHeight.value() *
+                (self.rectangleOutHeight.value() *
                 self.rectangleOutWidth.value() -
                 self.rectangleInHeight.value() *
-                self.rectangleInWidth.value())                     
-          
+                self.rectangleInWidth.value() )* (conFact ** 2))
+            
             self.sectionMoment.setValue(
                 (self.rectangleOutHeight.value() *
                  self.rectangleOutWidth.value() ** 3 -
                  self.rectangleInHeight.value() *
-                 self.rectangleInWidth.value() ** 3) / 12)
+                 self.rectangleInWidth.value() ** 3) / 12 *
+                (conFact ** 4))
             
     if self.iBeamButton.isChecked():
 
@@ -46,12 +47,13 @@ def callCalcSection(self):
                 self.iBeamHeight.value() * self.iBeamWidth.value() ** 3
                 - (self.iBeamHeight.value() - 2 *
                 self.iBeamFlange.value()) * (self.iBeamWidth.value() -
-                self.iBeamWeb.value()) ** 3) / 12)
+                self.iBeamWeb.value()) ** 3) / 12 * (conFact ** 2) )
 
             self.sectionMoment.setValue(
                 self.iBeamHeight.value() * self.iBeamWidth.value() -
                 (self.iBeamHeight.value() - 2 * self.iBeamFlange.value()
-                 ) * (self.iBeamWidth.value() - self.iBeamWeb.value()))
+                 ) * (self.iBeamWidth.value() - self.iBeamWeb.value()
+                * (conFact ** 4)))
             
     if self.uChannelButton.isChecked():
 
@@ -76,13 +78,14 @@ def callCalcSection(self):
             self.sectionArea.setValue(
                 self.channelHeight.value() *
                 self.channelWidth.value() -
-                self.flangeHeight.value() * self.flangeWidth.value())
+                self.flangeHeight.value() * self.flangeWidth.value()
+                * (conFact ** 2))
 
             self.sectionMoment.setValue(
                 (self.channelHeight.value() *
                 self.channelWidth.value() ** 3 -
                 self.flangeHeight.value() * self.flangeWidth.value()
-                ** 3) / 12)
+                ** 3) / 12 * (conFact ** 4))
         
     if self.roundButton.isChecked():
 
@@ -96,11 +99,11 @@ def callCalcSection(self):
             
             self.sectionArea.setValue(
                 math.pi * ((self.roundOD.value() / 2) ** 2 -
-                (self.roundID.value() / 2) ** 2))
+                (self.roundID.value() / 2) ** 2)  * (conFact ** 2))
 
             self.sectionMoment.setValue(
                 (math.pi * (self.roundOD.value() ** 4 -
-                self.roundID.value() ** 4) ) / 64)
+                self.roundID.value() ** 4) ) / 64 * (conFact ** 4))
 
     if self.hexButton.isChecked():
 
@@ -112,11 +115,13 @@ def callCalcSection(self):
 
         else:
 
-            self.sectionArea.setValue(self.hexLength.value() ** 2 * 3 /
-                                      2 * math.sqrt(3))
+            self.sectionArea.setValue(
+                self.hexLength.value() ** 2 * 3 / 2 * math.sqrt(3) *
+                (conFact ** 2))
 
             self.sectionMoment.setValue(
-                5 / 16 * math.sqrt(3) * self.hexLength.value() ** 4)
+                5 / 16 * math.sqrt(3) * self.hexLength.value() ** 4 *
+                (conFact ** 4))
 
     if self.tBeamButton.isChecked():
 
@@ -132,12 +137,12 @@ def callCalcSection(self):
             self.sectionArea.setValue(
                 (self.teeHeight.value() - self.teeFlange.value()) *
                 self.teeStem.value() + self.teeWidth.value() *
-                self.teeFlange.value())
+                self.teeFlange.value() * (conFact ** 2))
 
             self.sectionMoment.setValue(
                 self.teeStem.value() ** 3 * (self.teeHeight.value() -
                 self.teeFlange.value() ) / 12 + self.teeWidth.value() ** 3 *
-                self.teeFlange.value() / 12)
+                self.teeFlange.value() / 12 * (conFact ** 4))
 
     if self.angleButton.isChecked():
 
@@ -157,13 +162,16 @@ def callCalcSection(self):
 
             self.sectionArea.setValue(
                 (t * y ** 3 + a * (a - y) ** 3 - (a - t) * (a - y - t) ** 3)
-                / 3)            
+                / 3 * (conFact ** 2))
+
+            self.sectionMoment.setValue()
+
 
 def pickShape(self):
     
-    print('Step 2')
+##    print('Step 2')
 
-    #Hides geometry inputs based on radio button choice
+    # Hides geometry inputs based on radio button choice
 
     self.stackedWidget.show()
 
@@ -187,3 +195,79 @@ def pickShape(self):
 
     if self.angleButton.isChecked():
         self.stackedWidget.setCurrentIndex(6)
+
+def unitConv(self):
+
+    outFact = outputUnitLabel(self)
+
+    if str(self.inputUnit.currentText()) == 'Meters':
+
+        inFact = 1
+
+    elif str(self.inputUnit.currentText()) == 'cm':
+
+        inFact = 0.01
+
+    elif str(self.inputUnit.currentText()) == 'mm':
+
+        inFact = 0.001
+
+    elif str(self.inputUnit.currentText()) == 'Inches':
+
+        inFact = 0.0254
+
+    elif str(self.inputUnit.currentText()) == 'Feet':
+
+        inFact = 0.3048
+
+    convFact = inFact / outFact
+
+    return convFact
+
+def outputUnitLabel(self):
+    
+    if str(self.outputUnit.currentText()) == 'Meters':
+        
+        self.sectionArea.setSuffix(' m^2')
+
+        self.sectionMoment.setSuffix(' m^4')
+
+        outConvFact = 1
+
+    elif str(self.outputUnit.currentText()) == 'cm':
+
+        self.sectionArea.setSuffix(' cm^2')
+
+        self.sectionMoment.setSuffix(' cm^4')
+
+        outConvFact = 0.01
+
+    elif str(self.outputUnit.currentText()) == 'mm':
+
+        self.sectionArea.setSuffix(' mm^2')
+
+        self.sectionMoment.setSuffix(' mm^4')
+
+        outConvFact = 0.001
+
+    elif str(self.outputUnit.currentText()) == 'Inches':
+
+        self.sectionArea.setSuffix(' in^2')
+
+        self.sectionMoment.setSuffix(' in^4')
+
+        outConvFact = 0.0254
+
+    elif str(self.outputUnit.currentText()) == 'Feet':
+
+        self.sectionArea.setSuffix(' ft^2')
+
+        self.sectionMoment.setSuffix(' ft^4')
+
+        outConvFact = 0.3048
+
+    else:
+
+        print('Error: not yet defined.')
+
+    return outConvFact
